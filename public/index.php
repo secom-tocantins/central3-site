4693c5def6cc014b31cd697be5baecfcbbfe37de
Luan Almeida <luanlmd@gmail.com>
2012-11-23 14:18:20 -0200

    Carregndo itens do menu em todas as páginas

--- public/index.php
+++ public/index.php
@@ -20,15 +20,17 @@ $twig = $app['twig'];
 * App
 */
 
+$menu = $client->query('pagina.listar');
+
 /* Home */
-$app->get('/', function () use ($twig) {
-    return $twig->render('index.twig', array());
+$app->get('/', function () use ($twig, $menu) {
+    return $twig->render('index.twig', array('menu'=>$menu));
 });
 
 /* Listar notícias */
-$listar = function () use ($twig, $client, $app) {
+$listar = function () use ($twig, $menu, $client, $app) {
     $noticias = $client->byUri($app['request']->getPathInfo());
-    return $twig->render('noticias.twig', array('noticias' => $noticias));
+    return $twig->render('noticias.twig', array('menu'=>$menu, 'noticias' => $noticias));
 };
 $app->get('/noticia/', $listar);
 $app->get('/noticia/{ano}/', $listar);
@@ -36,27 +38,27 @@ $app->get('/noticia/{ano}/{mes}/', $listar);
 $app->get('/noticia/{ano}/{mes}/{dia}/', $listar);
 
 /* Visualizar notícia */
-$app->get('/noticia/{ano}/{mes}/{dia}/{slug}/', function () use ($twig, $client, $app) {
+$app->get('/noticia/{ano}/{mes}/{dia}/{slug}/', function () use ($twig, $menu, $client, $app) {
     $noticia = $client->byUri($app['request']->getPathInfo());
-    return $twig->render('noticia.twig', array('noticia' => $noticia));
+    return $twig->render('noticia.twig', array('menu'=>$menu, 'noticia' => $noticia));
 });
 
 /* Listar galerias */
-$app->get('/galeria/', function () use ($twig, $client, $app) {
+$app->get('/galeria/', function () use ($twig, $menu, $client, $app) {
     $galerias = $client->byUri($app['request']->getPathInfo());
-    return $twig->render('galerias.twig', array('galerias' => $galerias));
+    return $twig->render('galerias.twig', array('menu'=>$menu, 'galerias' => $galerias));
 });
 
 /* Visualizar galeria */
-$app->get('/galeria/{slug}/', function () use ($twig, $client, $app) {
+$app->get('/galeria/{slug}/', function () use ($twig, $menu, $client, $app) {
     $galeria = $client->byUri($app['request']->getPathInfo());
-    return $twig->render('galeria.twig', array('galeria' => $galeria));
+    return $twig->render('galeria.twig', array('menu'=>$menu, 'galeria' => $galeria));
 });
 
 /* Visualizar página */
-$app->get('/{slug}/', function () use ($twig, $client, $app) {
+$app->get('/{slug}/', function () use ($twig, $menu, $client, $app) {
     $pagina = $client->byUri($app['request']->getPathInfo());
-    return $twig->render('pagina.twig', array('pagina' => $pagina));
+    return $twig->render('pagina.twig', array('menu'=>$menu, 'pagina' => $pagina));
 });
 
 $app->run();
