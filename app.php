7cb33bbe0444e0dcc222b70103be73cbc2213186
Luan Almeida <luanlmd@gmail.com>
2012-12-11 14:33:32 -0200

    Um pouco de CSS e HTMl melhorado

--- app.php
+++ app.php
@@ -11,39 +11,43 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException,
 $app->before(function() use ($app) {
     $app['twig']->addGlobal('site', $app['client']->query('site.info')[0]);
     $app['twig']->addGlobal('menu', $app['client']->query('pagina.listar'));
+    $app['twig']->addGlobal('banners', $app['client']->query('banner.listar','area=75407'));
 });
 
 /* Home */
 $app->get('/', function (Silex\Application $app) {
-    $vars = array();
-    $vars['destaques'] = $app['client']->query('noticia.listar','destaque=s&temfoto=s&thumb=s&limite=3');
-    $vars['noticias'] = $app['client']->query('noticia.listar','limite=10');
-    return $app['twig']->render('index.twig', $vars);
-});
+    $destaques = $app['client']->query('noticia.listar','destaque=s&temfoto=s&thumb=s&limite=3');
+    $noticias = $app['client']->query('noticia.listar','limite=10&negar='.$destaques->getHead()->ids);
+    return $app['twig']->render('index.twig', array('destaques'=>$destaques, 'noticias' => $noticias));
+})->bind('index');
 
 /* Listar notícias */
 $listar = function (Silex\Application $app){
     try {
-        $noticias = $app['client']->byUri($app['request']->getPathInfo());
-        return $app['twig']->render('noticias.twig', array('noticias' => $noticias));
+        $pagina = $app['request']->get('pagina');
+        if (!is_numeric($pagina)) { $pagina = 1; }
+        $noticias = $app['client']->byUri($app['request']->getPathInfo(),"pagina={$pagina}&limite=20");
+        $pagina++;
+        $pagina = ($pagina < $noticias->getHead()->paginas)? $pagina : 0;
+        return $app['twig']->render('noticias.twig', array('noticias' => $noticias, 'proximaPagina' => $pagina));
     } catch(ApiException $e) {
         throw new NotFoundHttpException($e->getMessage(), $e, 404);
     }
 };
-$app->get('/noticia/', $listar);
-$app->get('/noticia/{ano}/', $listar);
-$app->get('/noticia/{ano}/{mes}/', $listar);
-$app->get('/noticia/{ano}/{mes}/{dia}/', $listar);
+$app->get('/noticia/', $listar)->bind('noticias');
+$app->get('/noticia/{ano}/', $listar)->bind('noticias.ano');
+$app->get('/noticia/{ano}/{mes}/', $listar)->bind('noticias.ano.mes');
+$app->get('/noticia/{ano}/{mes}/{dia}/', $listar)->bind('noticias.ano.mes.dia');
 
 /* Visualizar notícia */
 $app->get('/noticia/{ano}/{mes}/{dia}/{slug}/', function (Silex\Application $app) {
     try {
         $noticia = $app['client']->byUri($app['request']->getPathInfo());
-        return $app['twig']->render('noticia.twig', array('noticia' => $noticia));
+        return $app['twig']->render('noticia.twig', array('pagina' => $noticia));
     } catch(ApiException $e) {
         throw new NotFoundHttpException($e->getMessage(), $e, 404);
     }
-});
+})->bind('noticia');
 
 /* Listar galerias */
 $app->get('/galeria/', function (Silex\Application $app) {
@@ -53,7 +57,7 @@ $app->get('/galeria/', function (Silex\Application $app) {
     } catch(ApiException $e) {
         throw new NotFoundHttpException($e->getMessage(), $e, 404);
     }
-});
+})->bind('galerias');
 
 /* Visualizar galeria */
 $app->get('/galeria/{slug}/', function (Silex\Application $app) {
@@ -63,7 +67,7 @@ $app->get('/galeria/{slug}/', function (Silex\Application $app) {
     } catch(ApiException $e) {
         throw new NotFoundHttpException($e->getMessage(), $e, 404);
     }
-});
+})->bind('galeria');
 
 /* Visualizar página */
 $app->get('/{slug}/', function (Silex\Application $app) {
@@ -73,7 +77,7 @@ $app->get('/{slug}/', function (Silex\Application $app) {
     } catch(ApiException $e) {
         throw new NotFoundHttpException($e->getMessage(), $e, 404);
     }
-});
+})->bind('pagina');
 
 /* Tratando erros */
 $app->error(function (\Exception $e) use ($app) {
