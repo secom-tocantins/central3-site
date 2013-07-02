4b6af27178cdde02d90c23d4be149b86572c5032
Luan Almeida <luanlmd@gmail.com>
2013-07-02 14:45:51 -0300

    Scroll infinito melhorado

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('.modal').fancybox({helpers : {title: {type: 'outside'}}});
-    $('#content.noticias div').scroll4ever({
-            trigger:'#content.noticias a.next',
-            container: '#content.noticias div ul',
-            selector:'#content.noticias div ul li', // this is the selection made in the source url data
+    $('#content.noticias').scroll4ever({
+            trigger:'a.next',
+            container: 'ul',
+            selector:'ul li', // this is the selection made in the source url data
             distance:100, // distance to the end of window to trigger page load, if avoided, it will not auto trigger
             debug: true, // if you want some messages in console...
             start: function(){ $('#content.noticias a.next').html('Carregando mais notícias...');  }, // callback called when a new page load begins, good for loading messages
