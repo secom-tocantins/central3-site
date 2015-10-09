18e093c4d0caf1c32dd9be93f6623d109bb24589
Luan Almeida <luanlmd@gmail.com>
2015-10-09 16:44:14 -0300

    Correções no css e js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('#conteudo.noticias').scroll4ever({
+    $('#conteudo').scroll4ever({
         trigger:'a#more',
         container:'#noticias > div',
-        selector:'.media', // this is the selection made in the source url data
-        distance:500, // distance to the end of window to trigger page load, if avoided, it will not auto trigger
+        selector:'.item', // this is the selection made in the source url data
+        distance:1000, // distance to the end of window to trigger page load, if avoided, it will not auto trigger
         debug: false, // if you want some messages in console...
         start: function(){ $('#noticias a#more').html('Carregando...');  }, // callback called when a new page load begins, good for loading messages
         complete: function(){}, // callback called when a new page load ends
