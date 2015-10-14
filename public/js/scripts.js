a23b59a0db835b712b226222917dd0680a780e73
Luan Almeida <luanlmd@gmail.com>
2015-10-14 16:04:36 -0300

    Correções

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#conteudo').scroll4ever({
         trigger:'a#more',
         container:'#noticias > div',
-        selector:'.item', // this is the selection made in the source url data
+        selector:'.media', // this is the selection made in the source url data
         distance:1000, // distance to the end of window to trigger page load, if avoided, it will not auto trigger
         debug: false, // if you want some messages in console...
         start: function(){ $('#noticias a#more').html('Carregando...');  }, // callback called when a new page load begins, good for loading messages
